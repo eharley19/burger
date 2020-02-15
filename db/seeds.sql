bc8f2fde653f5e877d0cbcc8bd5372e875063ddd
Edward Harley <edharley19@gmail.com>
2020-02-15 15:54:29 -0500

    Setup and initialize burger database with seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("hamburger"), ("cheeseburger"), ("bacon cheeseburger"), ("double cheeseburger"), ("bacon double cheeseburger"), ("turkey burger"), ("veggie burger");
